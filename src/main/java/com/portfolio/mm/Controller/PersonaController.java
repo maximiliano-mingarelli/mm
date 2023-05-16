caa7fdf9f7dbff0613a8390d0af0a3abceecdcae
maximiliano-mingarelli <maximiliano.mingarelli@hotmail.com>
2023-05-15 21:36:17 -0300

    cambio de endpoints

--- src/main/java/com/portfolio/mm/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mm/Controller/PersonaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://front-end-c765b.web.app","http://localhost:4200"})
 @RequestMapping("/persona")
 public class PersonaController {
     @Autowired
--- src/main/java/com/portfolio/mm/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/mm/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = {"https://front-end-c765b.web.app/"})
 public class AuthController {
 
     @Autowired
